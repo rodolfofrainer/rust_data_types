d803ec526c30e414244c2668e2c2470bf2242e98
Rodolfo Frainer <91473305+rodolfofrainer@users.noreply.github.com>
2023-04-09 11:46:12 +0100

    create a variable of most data types

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // let x: i8 = 10;
+    // println!("{}", x);
+// 
+    // let y: u8 = 20;
+    // println!("{}", y);
+// 
+    // let decimal = 02_55;
+    // let hex = 0xff;
+    // let octal = 0o377;
+    // let binary =0b1111_1111;
+// 
+    // println!("{}",decimal);
+    // println!("{}", hex);
+    // println!("{}", octal);
+    // println!("{}", binary);
+// 
+    // let byte = b'A';
+    // println!("{}",byte)
+
+    let _x =2.0; 
+    let _y: f32 = 1.0;
+
+    let _t = true;
+    let _f = false;
+
+    let c = 'c';
+    println!("{}", c)
+
+}
